8dce69a9fc143f2916a2f7b8b5357d2f30cdfcc3
Your Name <you@example.com>
2022-03-24 07:59:07 +0000

    Add Sign Up

--- Web/Types.hs
+++ Web/Types.hs
@@ -20,3 +20,13 @@ data SessionsController
     | CreateSessionAction
     | DeleteSessionAction
     deriving (Eq, Show, Data)
+
+data UsersController
+    = UsersAction
+    | NewUserAction
+    | ShowUserAction { userId :: !(Id User) }
+    | CreateUserAction
+    | EditUserAction { userId :: !(Id User) }
+    | UpdateUserAction { userId :: !(Id User) }
+    | DeleteUserAction { userId :: !(Id User) }
+    deriving (Eq, Show, Data)
