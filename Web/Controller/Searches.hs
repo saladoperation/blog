178415d55311f4c2cb64cdc8e85d310215723ea3
Your Name <you@example.com>
2022-04-20 00:53:10 +0000

    Add search result

--- Web/Controller/Searches.hs
+++ Web/Controller/Searches.hs
@@ -5,5 +5,5 @@ import Web.View.Searches.Show
 
 instance Controller SearchesController where
     action ShowSearchAction { keyword } = do
+        entries :: [Entry] <- sqlQuery "SELECT * FROM entries ORDER BY SIMILARITY(METAPHONE(text, 4), METAPHONE(?, 4)) DESC" (Only keyword)
         render ShowView { .. }
-
